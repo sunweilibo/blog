cd8330f79b31b9c191c1633defa377d85fa986a3
sunweili <sunweili@yy.com>
2021-04-21 08:22:42 +0800

    fix:filter file

--- docs/.vuepress/tools.js
+++ docs/.vuepress/tools.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 function getFiles(prefix, filePath) {
   const readPath = path.join(__dirname, filePath)
-  const files = fs.readdirSync(readPath)
+  const files = fs.readdirSync(readPath).filter(file => file.endsWith('.md'))
   const res = files.map(file => prefix + file.split('.')[0])
   return res
 }
